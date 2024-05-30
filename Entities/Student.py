f2ba1260de6f9eb6bc459bfef5737f259a55564c
kaczorekt <kaczorekt6@gmail.com>
2024-05-30 21:40:00 +0200

    First Commit

---
+++ Entities/Student.py
@@ -0,0 +1,5 @@
+class Student:
+    def __init__(self, imie, nazwisko):
+        self.i = imie
+        self.n = nazwisko
+
---
+++ Program/main.py
@@ -0,0 +1,8 @@
+from Entities.Student import Student
+from GUI.Window import Window
+
+
+Jacek = Student("Jacek", "Debil")
+window = Window
+
+print(Jacek.i)
